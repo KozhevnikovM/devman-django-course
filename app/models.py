95d87fd05cf4dcc0b2099847807702e645def315
Administrator <admin@example.com>
2021-03-03 14:25:12 +0300

    added image model

--- app/models.py
+++ app/models.py
@@ -7,3 +7,19 @@ class Place(models.Model):
     description_long = models.TextField()
     coordinates_lng = models.FloatField()
     coordinates_lat = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=200, primary_key=True)
+    order = models.SmallIntegerField(unique=True, default=0)
+    place = models.ForeignKey(to='Place', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.order} {self.name}'
+    
+    class Meta:
+        ordering = ['order']
+        
