bef1f9a2450ce84a76d92362dcdc9790deaf7d97
Administrator <admin@example.com>
2021-03-03 16:56:20 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from app.models import Place, Image
 
 def show_index(request):
-    return render(request, 'index.html')
+    places = Place.objects.all()
+    images = Image.objects.all().filter(place=places[0])
+
+    value = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.coordinates_lng, place.coordinates_lat]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": place.title,
+            "detailsUrl": "static/places/moscow_legends.json"
+
+          }
+        } for place in places
+      ]
+    }
+    context = {
+        'value': value
+    }
+    return render(request, context=context, template_name='index.html')
