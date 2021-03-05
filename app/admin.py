f09ef21a8e9e23497829b64222c377ee1e8bed12
Administrator <admin@example.com>
2021-03-05 13:11:59 +0300

    added preview

--- app/admin.py
+++ app/admin.py
@@ -1,15 +1,33 @@
 from django.contrib import admin
 from .models import Place, Image
-# Register your models here.
-# admin.site.register(Place)
-admin.site.register(Image)
+from django.utils.html import mark_safe, format_html
 
+# admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
 
 class ImageInline(admin.TabularInline):
+    readonly_fields = ('get_preview',)
+    fields = ('image', 'get_preview', 'order')
+    
+    def get_preview(self, obj):
+        return format_html('<img src="{url}" height={height} />'.format(
+            url = obj.image.url,
+            height=200,
+            )
+        )
+    
+    get_preview.short_description = 'preview'
     model = Image
+    
+    
+    
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline
     ]
+
