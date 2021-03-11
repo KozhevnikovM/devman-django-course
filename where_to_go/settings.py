63250812f76222cb88dd5ed4f5ad74df3eaf3c58
Administrator <admin@example.com>
2021-03-11 10:47:52 +0300

    removed default from allowe hosts, updated requirements

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY', default='InsecureKey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='[*]')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 if not DEBUG:
     CSRF_COOKIE_SECURE = False
