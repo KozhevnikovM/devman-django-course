ae213c26732b872ce192be250c48af3ec983ba4e
Administrator <admin@example.com>
2021-03-15 12:44:35 +0300

    removed functions with json loadin

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -4,8 +4,7 @@ from places.models import Place, Image
 import json
 from urllib.parse import urlparse
 from django.core.files.base import ContentFile
-from django.core.validators import URLValidator
-from django.core.exceptions import ValidationError
+
 
 class Command(BaseCommand):
     help = 'Import place from json'
@@ -14,15 +13,6 @@ class Command(BaseCommand):
         group = parser.add_mutually_exclusive_group(required=True)
         group.add_argument('--url', type=str, help='Url to global json file')
         group.add_argument('--path', type=str, help='Path to local json file')
-
-    def load_json_from_file(self, filepath):
-        with open(filepath, 'r', encoding='utf-8') as file:
-            return json.loads(file.read())
-
-    def load_json_from_url(self, url):
-        response = requests.get(url)
-        if response.ok:
-            return response.json()
 
     def get_place_details(self, json_data):
         place_details = {
@@ -36,7 +26,15 @@ class Command(BaseCommand):
         return place_details
 
     def handle(self, *args, **options):
-        json_data = self.load_json_from_url(options['url']) if options['url'] else self.load_json_from_file(options['path'])
+        if options['url']:
+            response = requests.get(options['url'])
+            response.raise_for_status()
+            json_data = response.json()
+        
+        if options['path']:
+            with open(options['path'], 'r', encoding='utf-8') as file:
+                json_data = json.loads(file.read())
+        
         place_details = self.get_place_details(json_data)
         urls = place_details.pop('imgs', None)
         place, get_place_result = Place.objects.get_or_create(title=place_details['title'], defaults=place_details)
